f2ad14adee1ca6c194a15f871e8493a11543d2fd
Franklin Herman <fherman@macbookair.mynetworksettings.com>
2025-05-08 21:52:09 -0400

    more nets

--- service-worker.js
+++ service-worker.js
@@ -17,6 +17,7 @@ const urlsToCache = [
     '/nets/truncated-tetrahedron-top.json',
     '/nets/truncated-octahedron-top.json',
     '/nets/truncated-cuboctahedron-top.json',
+    '/nets/truncated-dodecahedron-top.json',
     '/nets/snub-cube-top.json',
     '/nets/snub-dodecahedron-top.json',
     '/icons/192.png',
